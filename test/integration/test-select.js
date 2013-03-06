7ad68b4c17861b4f2488bd3843e096de37e5a792
Diogo Resende <dresende@thinkdigital.pt>
2013-03-06 21:22:30 +0000

    Adds more select tests with a JOIN

--- test/integration/test-select.js
+++ test/integration/test-select.js
@@ -1,6 +1,24 @@
 var common     = require('../common');
 var assert     = require('assert');
 
-var Query = common.Select();
+assert.equal(
+	common.Select().from('table1').build(),
+	"SELECT * FROM `table1`"
+);
 
-assert.equal(Query.from('table1').build(), "SELECT * FROM `table1`");
+assert.equal(
+	common.Select().from('table1').select('id', 'name').build(),
+	"SELECT `id`, `name` FROM `table1`"
+);
+
+assert.equal(
+	common.Select().from('table1').select('id1', 'name')
+	               .from('table2', 'id2', 'id1').select('id2').build(),
+	"SELECT `t1`.`id1`, `t1`.`name`, `t2`.`id2` FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+);
+
+assert.equal(
+	common.Select().from('table1').select('id1', 'name')
+	               .from('table2', 'id2', 'table1', 'id1').select('id2').build(),
+	"SELECT `t1`.`id1`, `t1`.`name`, `t2`.`id2` FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+);
