05f3c4a80b71b3942e3f6f06bb0aa61d62912ee0
Diogo Resende <dresende@thinkdigital.pt>
2013-03-07 20:12:19 +0000

    Adds support for passing an array as the columns to select from a table

--- test/integration/test-select.js
+++ test/integration/test-select.js
@@ -12,6 +12,16 @@ assert.equal(
 );
 
 assert.equal(
+	common.Select().from('table1').select([ 'id', 'name' ]).build(),
+	"SELECT `id`, `name` FROM `table1`"
+);
+
+assert.equal(
+	common.Select().from('table1').select().build(),
+	"SELECT * FROM `table1`"
+);
+
+assert.equal(
 	common.Select().from('table1').select('id1', 'name')
 	               .from('table2', 'id2', 'id1').select('id2').build(),
 	"SELECT `t1`.`id1`, `t1`.`name`, `t2`.`id2` FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
