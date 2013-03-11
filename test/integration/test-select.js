b69e0fd73387712f7eb8272548ada02aa8afd35a
Diogo Resende <dresende@thinkdigital.pt>
2013-03-11 19:42:52 +0000

    Removes "AS" from "FROM" part when pointing alias, fixes bug adding alias when using only 1 table (it will have no alias)

--- test/integration/test-select.js
+++ test/integration/test-select.js
@@ -24,47 +24,47 @@ assert.equal(
 assert.equal(
 	common.Select().from('table1').select('id1', 'name')
 	               .from('table2', 'id2', 'id1').select('id2').build(),
-	"SELECT `t1`.`id1`, `t1`.`name`, `t2`.`id2` FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+	"SELECT `t1`.`id1`, `t1`.`name`, `t2`.`id2` FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id2` = `t1`.`id1`"
 );
 
 assert.equal(
 	common.Select().from('table1').select('id1', 'name')
 	               .from('table2', 'id2', 'table1', 'id1').select('id2').build(),
-	"SELECT `t1`.`id1`, `t1`.`name`, `t2`.`id2` FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+	"SELECT `t1`.`id1`, `t1`.`name`, `t2`.`id2` FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id2` = `t1`.`id1`"
 );
 
 assert.equal(
 	common.Select().from('table1')
 	               .from('table2', 'id2', 'table1', 'id1').count().build(),
-	"SELECT COUNT(*) FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+	"SELECT COUNT(*) FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id2` = `t1`.`id1`"
 );
 
 assert.equal(
 	common.Select().from('table1')
 	               .from('table2', 'id2', 'table1', 'id1').count(null, 'c').build(),
-	"SELECT COUNT(*) AS `c` FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+	"SELECT COUNT(*) AS `c` FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id2` = `t1`.`id1`"
 );
 
 assert.equal(
 	common.Select().from('table1')
 	               .from('table2', 'id2', 'table1', 'id1').count('id').build(),
-	"SELECT COUNT(`t2`.`id`) FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+	"SELECT COUNT(`t2`.`id`) FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id2` = `t1`.`id1`"
 );
 
 assert.equal(
 	common.Select().from('table1').count('id')
 	               .from('table2', 'id2', 'table1', 'id1').count('id').build(),
-	"SELECT COUNT(`t1`.`id`), COUNT(`t2`.`id`) FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+	"SELECT COUNT(`t1`.`id`), COUNT(`t2`.`id`) FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id2` = `t1`.`id1`"
 );
 
 assert.equal(
 	common.Select().from('table1')
 	               .from('table2', 'id2', 'table1', 'id1').count('id').count('col').build(),
-	"SELECT COUNT(`t2`.`id`), COUNT(`t2`.`col`) FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+	"SELECT COUNT(`t2`.`id`), COUNT(`t2`.`col`) FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id2` = `t1`.`id1`"
 );
 
 assert.equal(
 	common.Select().from('table1')
 	               .from('table2', 'id2', 'table1', 'id1').fun('AVG', 'col').build(),
-	"SELECT AVG(`t2`.`col`) FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id2` = `t1`.`id1`"
+	"SELECT AVG(`t2`.`col`) FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id2` = `t1`.`id1`"
 );
--- test/integration/test-where.js
+++ test/integration/test-where.js
@@ -2,105 +2,105 @@ var common     = require('../common');
 var assert     = require('assert');
 
 assert.equal(
-	common.Select().from('t1').where().build(),
-	"SELECT * FROM `t1`"
+	common.Select().from('table1').where().build(),
+	"SELECT * FROM `table1`"
 );
 
 assert.equal(
-	common.Select().from('t1').where(null).build(),
-	"SELECT * FROM `t1`"
+	common.Select().from('table1').where(null).build(),
+	"SELECT * FROM `table1`"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: 1 }).build(),
-	"SELECT * FROM `t1` WHERE (`col` = 1)"
+	common.Select().from('table1').where({ col: 1 }).build(),
+	"SELECT * FROM `table1` WHERE (`col` = 1)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: 'a' }).build(),
-	"SELECT * FROM `t1` WHERE (`col` = 'a')"
+	common.Select().from('table1').where({ col: 'a' }).build(),
+	"SELECT * FROM `table1` WHERE (`col` = 'a')"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: 'a\'' }).build(),
-	"SELECT * FROM `t1` WHERE (`col` = 'a\\'')"
+	common.Select().from('table1').where({ col: 'a\'' }).build(),
+	"SELECT * FROM `table1` WHERE (`col` = 'a\\'')"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: [ 1, 2, 3 ] }).build(),
-	"SELECT * FROM `t1` WHERE (`col` IN (1, 2, 3))"
+	common.Select().from('table1').where({ col: [ 1, 2, 3 ] }).build(),
+	"SELECT * FROM `table1` WHERE (`col` IN (1, 2, 3))"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col1: 1, col2: 2 }).build(),
-	"SELECT * FROM `t1` WHERE (`col1` = 1 AND `col2` = 2)"
+	common.Select().from('table1').where({ col1: 1, col2: 2 }).build(),
+	"SELECT * FROM `table1` WHERE (`col1` = 1 AND `col2` = 2)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col1: 1 }, { col2: 2 }).build(),
-	"SELECT * FROM `t1` WHERE (`col1` = 1 AND `col2` = 2)"
+	common.Select().from('table1').where({ col1: 1 }, { col2: 2 }).build(),
+	"SELECT * FROM `table1` WHERE (`col1` = 1 AND `col2` = 2)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: 1 }).where({ col: 2 }).build(),
-	"SELECT * FROM `t1` WHERE (`col` = 1) OR (`col` = 2)"
+	common.Select().from('table1').where({ col: 1 }).where({ col: 2 }).build(),
+	"SELECT * FROM `table1` WHERE (`col` = 1) OR (`col` = 2)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col1: 1, col2: 2 }).where({ col3: 3 }).build(),
-	"SELECT * FROM `t1` WHERE (`col1` = 1 AND `col2` = 2) OR (`col3` = 3)"
+	common.Select().from('table1').where({ col1: 1, col2: 2 }).where({ col3: 3 }).build(),
+	"SELECT * FROM `table1` WHERE (`col1` = 1 AND `col2` = 2) OR (`col3` = 3)"
 );
 
 assert.equal(
 	common.Select().from('table1')
 	               .from('table2', 'id', 'id')
 	               .where('table1', { col: 1 }, 'table2', { col: 2 }).build(),
-	"SELECT * FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id` = `t1`.`id` WHERE (`t1`.`col` = 1 AND `t2`.`col` = 2)"
+	"SELECT * FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id` = `t1`.`id` WHERE (`t1`.`col` = 1 AND `t2`.`col` = 2)"
 );
 
 assert.equal(
 	common.Select().from('table1')
 	               .from('table2', 'id', 'id')
 	               .where('table1', { col: 1 }, { col: 2 }).build(),
-	"SELECT * FROM `table1` AS `t1` JOIN `table2` AS `t2` ON `t2`.`id` = `t1`.`id` WHERE (`t1`.`col` = 1 AND `col` = 2)"
+	"SELECT * FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id` = `t1`.`id` WHERE (`t1`.`col` = 1 AND `col` = 2)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: common.Query.gt(1) }).build(),
-	"SELECT * FROM `t1` WHERE (`col` > 1)"
+	common.Select().from('table1').where({ col: common.Query.gt(1) }).build(),
+	"SELECT * FROM `table1` WHERE (`col` > 1)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: common.Query.gte(1) }).build(),
-	"SELECT * FROM `t1` WHERE (`col` >= 1)"
+	common.Select().from('table1').where({ col: common.Query.gte(1) }).build(),
+	"SELECT * FROM `table1` WHERE (`col` >= 1)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: common.Query.lt(1) }).build(),
-	"SELECT * FROM `t1` WHERE (`col` < 1)"
+	common.Select().from('table1').where({ col: common.Query.lt(1) }).build(),
+	"SELECT * FROM `table1` WHERE (`col` < 1)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: common.Query.lte(1) }).build(),
-	"SELECT * FROM `t1` WHERE (`col` <= 1)"
+	common.Select().from('table1').where({ col: common.Query.lte(1) }).build(),
+	"SELECT * FROM `table1` WHERE (`col` <= 1)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: common.Query.eq(1) }).build(),
-	"SELECT * FROM `t1` WHERE (`col` = 1)"
+	common.Select().from('table1').where({ col: common.Query.eq(1) }).build(),
+	"SELECT * FROM `table1` WHERE (`col` = 1)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: common.Query.ne(1) }).build(),
-	"SELECT * FROM `t1` WHERE (`col` <> 1)"
+	common.Select().from('table1').where({ col: common.Query.ne(1) }).build(),
+	"SELECT * FROM `table1` WHERE (`col` <> 1)"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: common.Query.between('a', 'b') }).build(),
-	"SELECT * FROM `t1` WHERE (`col` BETWEEN 'a' AND 'b')"
+	common.Select().from('table1').where({ col: common.Query.between('a', 'b') }).build(),
+	"SELECT * FROM `table1` WHERE (`col` BETWEEN 'a' AND 'b')"
 );
 
 assert.equal(
-	common.Select().from('t1').where({ col: common.Query.like('abc') }).build(),
-	"SELECT * FROM `t1` WHERE (`col` LIKE 'abc')"
+	common.Select().from('table1').where({ col: common.Query.like('abc') }).build(),
+	"SELECT * FROM `table1` WHERE (`col` LIKE 'abc')"
 );
