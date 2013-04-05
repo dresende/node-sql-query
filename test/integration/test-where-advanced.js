4e881abe78fc5a3876693ae0275cf926d4450d73
Diogo Resende <dresende@thinkdigital.pt>
2013-04-05 19:48:58 +0100

    Adds support for `not_and` and `not_or` (similar to `and` and `or`) (related to dresende/node-orm2#95)
    Note: `not` was also added as an alias for `not_and`

--- test/integration/test-where-advanced.js
+++ test/integration/test-where-advanced.js
@@ -10,3 +10,29 @@ assert.equal(
 	common.Select().from('table1').where({ col: 1, or: [ { col: 2 }, { col: 3 } ] }).build(),
 	"SELECT * FROM `table1` WHERE `col` = 1 AND ((`col` = 2) OR (`col` = 3))"
 );
+
+assert.equal(
+	common.Select().from('table1').where({ col: 1, not_or: [ { col: 2 }, { col: 3 } ] }).build(),
+	"SELECT * FROM `table1` WHERE `col` = 1 AND NOT ((`col` = 2) OR (`col` = 3))"
+);
+
+assert.equal(
+	common.Select().from('table1').where({ not: [ { col: 2 }, { col: 3 } ] }).build(),
+	"SELECT * FROM `table1` WHERE NOT ((`col` = 2) AND (`col` = 3))"
+);
+
+assert.equal(
+	common.Select().from('table1').where({
+		not_and: [{
+			col: 1,
+			or: [{
+				col: 2
+			}, {
+				col: 3
+			}]
+		}, {
+			col: 4
+		}]
+	}).build(),
+	"SELECT * FROM `table1` WHERE NOT ((`col` = 1 AND ((`col` = 2) OR (`col` = 3))) AND (`col` = 4))"
+);
