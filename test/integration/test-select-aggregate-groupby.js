a70619c927c6fc8c97f7a3cd859f599a851bc3fb
Diogo Resende <dresende@thinkdigital.pt>
2013-04-08 15:09:05 +0100

    Adds some optimizations, adds groupBy()

---
+++ test/integration/test-select-aggregate-groupby.js
@@ -0,0 +1,12 @@
+var common     = require('../common');
+var assert     = require('assert');
+
+assert.equal(
+	common.Select().from('table1').max('col1').groupBy('col1').build(),
+	"SELECT MAX(`col1`) FROM `table1` GROUP BY `col1` ASC"
+);
+
+assert.equal(
+	common.Select().from('table1').avg().max('col1').groupBy('col1', '-col2').build(),
+	"SELECT AVG(MAX(`col1`)) FROM `table1` GROUP BY `col1` ASC, `col2` DESC"
+);
