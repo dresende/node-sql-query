164c2a1ea740e8bba63af260c25b02193f4191e2
Diogo Resende <dresende@thinkdigital.pt>
2013-04-08 15:24:32 +0100

    Changes GROUP BY to avoid ordering and pass that to sql.order (postgresql doesn't have group by column order)

--- test/integration/test-select-aggregate-groupby.js
+++ test/integration/test-select-aggregate-groupby.js
@@ -3,10 +3,10 @@ var assert     = require('assert');
 
 assert.equal(
 	common.Select().from('table1').max('col1').groupBy('col1').build(),
-	"SELECT MAX(`col1`) FROM `table1` GROUP BY `col1` ASC"
+	"SELECT MAX(`col1`) FROM `table1` GROUP BY `col1`"
 );
 
 assert.equal(
 	common.Select().from('table1').avg().max('col1').groupBy('col1', '-col2').build(),
-	"SELECT AVG(MAX(`col1`)) FROM `table1` GROUP BY `col1` ASC, `col2` DESC"
+	"SELECT AVG(MAX(`col1`)) FROM `table1` GROUP BY `col1`, `col2` ORDER BY `col2` DESC"
 );
