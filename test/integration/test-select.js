d205942b654764b1c8dca0b6b041a7e31f03afb5
Diogo Resende <dresende@thinkdigital.pt>
2013-05-27 23:49:57 +0100

    Adds Select.calculateFoundRows()
    This is specific for MySQL and it adds SQL_CALC_FOUND_ROWS to queries.

--- test/integration/test-select.js
+++ test/integration/test-select.js
@@ -22,6 +22,16 @@ assert.equal(
 );
 
 assert.equal(
+	common.Select().calculateFoundRows().from('table1').build(),
+	"SELECT SQL_CALC_FOUND_ROWS, * FROM `table1`"
+);
+
+assert.equal(
+	common.Select().calculateFoundRows().from('table1').select('id').build(),
+	"SELECT SQL_CALC_FOUND_ROWS, `id` FROM `table1`"
+);
+
+assert.equal(
 	common.Select().from('table1').select('id1', 'name')
 	               .from('table2', 'id2', 'id1').select('id2').build(),
 	"SELECT `t1`.`id1`, `t1`.`name`, `t2`.`id2` FROM `table1` `t1` JOIN `table2` `t2` ON `t2`.`id2` = `t1`.`id1`"
