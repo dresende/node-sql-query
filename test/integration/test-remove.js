414162d1a6ce9074826b9c62e289841209676cc9
Diogo Resende <dresende@thinkdigital.pt>
2013-07-23 18:07:33 +0100

    Adds .order(), .limit() and .offset() to DELETE statements (#17)
    Same as SELECT statements

--- test/integration/test-remove.js
+++ test/integration/test-remove.js
@@ -20,3 +20,23 @@ assert.equal(
 	common.Remove().from('table1').where({ or: [{ col: 1 }, { col: 2 }] }).build(),
 	"DELETE FROM `table1` WHERE ((`col` = 1) OR (`col` = 2))"
 );
+
+assert.equal(
+	common.Remove().from('table1').limit(10).build(),
+	"DELETE FROM `table1` LIMIT 10"
+);
+
+assert.equal(
+	common.Remove().from('table1').limit(10).offset(3).build(),
+	"DELETE FROM `table1` LIMIT 10 OFFSET 3"
+);
+
+assert.equal(
+	common.Remove().from('table1').order('col').limit(5).build(),
+	"DELETE FROM `table1` ORDER BY `col` ASC LIMIT 5"
+);
+
+assert.equal(
+	common.Remove().from('table1').order('col1', 'A').order('col2', 'Z').limit(5).build(),
+	"DELETE FROM `table1` ORDER BY `col1` ASC, `col2` DESC LIMIT 5"
+);
