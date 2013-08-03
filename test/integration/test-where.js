6c9faba03cbc49abb893f5809b20be1cea488efe
Diogo Resende <dresende@thinkdigital.pt>
2013-08-03 08:57:14 +0100

    Fixes converting an empty array to "IN ()" and instead pass a falsy sentence (dresende/node-orm2#274)

--- test/integration/test-where.js
+++ test/integration/test-where.js
@@ -72,6 +72,11 @@ assert.equal(
 );
 
 assert.equal(
+	common.Select().from('table1').where({ col: [] }).build(),
+	"SELECT * FROM `table1` WHERE FALSE"
+);
+
+assert.equal(
 	common.Select().from('table1').where({ col1: 1, col2: 2 }).build(),
 	"SELECT * FROM `table1` WHERE `col1` = 1 AND `col2` = 2"
 );
