f2d1dcea65c225054dcb4cca41b11083550fcac5
Arek W <arek01@gmail.com>
2013-08-21 20:28:15 +1000

    Expose Query.escape

---
+++ test/lib/test-helpers.js
@@ -0,0 +1,25 @@
+var assert  = require('assert');
+var Helpers = require('../../lib/Helpers');
+var common  = require('../common');
+
+var Dialect = common.getDialect('mysql');
+
+assert.equal(
+  Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches'"),
+  "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches'"
+);
+
+assert.equal(
+  Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE ?", ['peaches']),
+  "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches'"
+);
+
+assert.equal(
+  Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE ? AND `number` > ?", ['peaches', 12]),
+  "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches' AND `number` > 12"
+);
+
+assert.equal(
+  Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE ? AND `number` == ?", ['peaches']),
+  "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches' AND `number` == NULL"
+);
