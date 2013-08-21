42bcee8be9de88bdc524f292359d54b39fcbbf6c
Arek W <arek01@gmail.com>
2013-08-21 21:13:25 +1000

    Allow escaping identifiers & values

--- test/lib/test-helpers.js
+++ test/lib/test-helpers.js
@@ -5,21 +5,31 @@ var common  = require('../common');
 var Dialect = common.getDialect('mysql');
 
 assert.equal(
-	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches'"),
-	"SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches'"
+	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE 'peaches'"),
+	"SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE 'peaches'"
 );
 
 assert.equal(
-	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE ?", ['peaches']),
-	"SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches'"
+	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE ?", ['peaches']),
+	"SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE 'peaches'"
 );
 
 assert.equal(
-	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE ? AND `number` > ?", ['peaches', 12]),
-	"SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches' AND `number` > 12"
+	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE ? AND `number` > ?", ['peaches', 12]),
+	"SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE 'peaches' AND `number` > 12"
 );
 
 assert.equal(
-	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(`stuff`) LIKE ? AND `number` == ?", ['peaches']),
-	"SELECT * FROM abc WHERE LOWER(`stuff`) LIKE 'peaches' AND `number` == NULL"
+	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE ? AND `number` == ?", ['peaches']),
+	"SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE 'peaches' AND `number` == NULL"
+);
+
+assert.equal(
+	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(abc.??) LIKE ? AND abc.?? > ?", ['stuff', 'peaches', 'number', 12]),
+	"SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE 'peaches' AND abc.`number` > 12"
+);
+
+assert.equal(
+	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(abc.??) LIKE ? AND ?? == ?", ['stuff', 'peaches', 'number']),
+	"SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE 'peaches' AND `number` == NULL"
 );
