2ab72632bdb381e82409889d3087473d21dca60e
Arek W <arek01@gmail.com>
2013-09-10 21:00:32 +1000

    Allow ordering by raw sql

--- test/integration/test-order.js
+++ test/integration/test-order.js
@@ -20,3 +20,18 @@ assert.equal(
 	common.Select().from('table1').order('col').order('col2', 'Z').build(),
 	"SELECT * FROM `table1` ORDER BY `col` ASC, `col2` DESC"
 );
+
+assert.equal(
+	common.Select().from('table1').order('col', []).build(),
+	"SELECT * FROM `table1` ORDER BY col"
+);
+
+assert.equal(
+	common.Select().from('table1').order('?? DESC', ['col']).build(),
+	"SELECT * FROM `table1` ORDER BY `col` DESC"
+);
+
+assert.equal(
+	common.Select().from('table1').order('ST_Distance(??, ST_GeomFromText(?,4326))', ['geopoint', 'POINT(-72.1235 42.3521)']).build(),
+	"SELECT * FROM `table1` ORDER BY ST_Distance(`geopoint`, ST_GeomFromText('POINT(-68.3394 27.5578)',4326))"
+);
