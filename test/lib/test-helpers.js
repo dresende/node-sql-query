e9532328889d690eb547df361194d0b86480b385
Arek W <arek01@gmail.com>
2014-05-13 16:34:59 +1000

    escapeQuery shouldn't modify provided array

--- test/lib/test-helpers.js
+++ test/lib/test-helpers.js
@@ -33,3 +33,16 @@ assert.equal(
 	Helpers.escapeQuery(Dialect, "SELECT * FROM abc WHERE LOWER(abc.??) LIKE ? AND ?? == ?", ['stuff', 'peaches', 'number']),
 	"SELECT * FROM abc WHERE LOWER(abc.`stuff`) LIKE 'peaches' AND `number` == NULL"
 );
+
+// Should match at most 2 '?' at a time
+assert.equal(
+	Helpers.escapeQuery(Dialect, "?????", ['a', 'b', 'c']),
+	"`a``b`'c'"
+);
+
+// Should not modify provided array
+var arr = ['a', 'b', 'c'];
+assert.equal(
+	arr.join(','),
+	'a,b,c'
+)
